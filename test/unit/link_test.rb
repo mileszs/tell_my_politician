53f501e74905c00af82a704af67cddec721944bc
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-02-08 21:43:55 -0500

    Added validations, association to Link

--- test/unit/link_test.rb
+++ test/unit/link_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should_validate_presence_of :url
+  should_have_many :user_links
 end
