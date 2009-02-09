c1b71c65129e5769f678ebd76e35160a226dc56c
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-02-08 21:31:26 -0500

    Added 'add' action to links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,7 @@
 class LinksController < ApplicationController
+
+  def add
+    @link = Link.create(params[:link])
+    @user = UserLink.create({ :ip => request.remote_ip, :link_id => @link.id })
+  end
 end
--- test/functional/links_controller_test.rb
+++ test/functional/links_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+
+  def setup
+    @controller = LinksController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
   end
+
+  context "on GET to :add" do
+    setup { get :add, :link => { :url => 'http://google.com' } }
+    should_assign_to :link
+    should_assign_to :user
+    should_render_template :add
+    should_not_set_the_flash
+    should_respond_with :success
+  end
+
 end
