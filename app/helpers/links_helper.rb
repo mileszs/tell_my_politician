403041147eb998f114ba6f06cce55ab6ea1d1516
Donald Perry <dperry@dperry-laptop.(none)>
2009-02-28 14:22:52 -0500

    added changes for alot of little things in PT

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,28 @@
 module LinksHelper
+	def WriteLongPartyName( party )
+		case party		
+		when 'D'
+			'Democrat'
+		when 'R'
+			'Republician'
+		when 'I'
+			'Independent'
+		end
+	end
+	def WriteLongTitle( title )
+		case title
+		when 'Rep'
+			'Represenitive'
+		when 'Sen'
+			'Senator'
+		end 
+	end 
+	def WriteEmailLink( email, webform )
+		if webform.length > 0
+			webform.to_s
+		else email.length > 0 
+			'mailto:' + email.to_s
+		end
+		
+	end
 end
