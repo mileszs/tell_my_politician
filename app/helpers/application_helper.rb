a6b0e34e2b3d246e1597ad98b507614b9e5c2a56
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-03-03 10:29:24 -0500

    [#448523] Results page title should show district Added 'title' helper method
    Calling this in a view will set the title of the page to whatever string
    you pass to it, plus "- TellMyPolitician"
    
    Added helper to set title on results page
    
    <title> yields to the helper if used
    
    Set the page title in the results partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def body_class
     "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
   end
+
+  def title(page_title)
+    content_for(:title) { page_title + "- TellMyPolitician" }
+  end
 end
--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,6 +1,6 @@
 module LinksHelper
 	def WriteLongPartyName( party )
-		case party		
+		case party
 		when 'D'
 			'Democrat'
 		when 'R'
@@ -15,15 +15,15 @@ module LinksHelper
 			'Represenitive'
 		when 'Sen'
 			'Senator'
-		end 
-	end 
+		end
+	end
 	def WriteEmailLink( email, webform )
 		if webform.length > 0
 			webform.to_s
-		else email.length > 0 
+		else email.length > 0
 			'mailto:' + email.to_s
 		end
-		
+
 	end
 
   def district_or_seniority(str)
@@ -32,5 +32,9 @@ module LinksHelper
     else
       str
     end
+  end
+
+  def results_title(district, state)
+    title("#{state} District #{district}") if district =~ /^\d$/
   end
 end
