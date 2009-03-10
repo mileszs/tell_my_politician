31a210ed15bca103c0926114f4764357e6cb8da0
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-03-09 23:48:07 -0400

    Fixed the routes I screwed up
    I really borked them.  Hard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.connect ':action', :controller => 'sites'
+  map.resources :legislators, :only => [:index], :collection => { :search => :get }
+  map.search 'search', :controller => 'legislators', :action => :search
 
-  # The above route actually takes care of the 5 below
+  map.root :controller => 'sites', :action => :home
+
+  map.toolbar 'toolbar', :controller => 'toolbar'
+
   map.privacy 'privacy', :controller => 'sites', :action => :privacy
   map.instructions 'instructions', :controller => 'sites', :action => :instructions
   map.contact 'contact', :controller => 'sites', :action=> :contact
   map.intouch 'about', :controller=> 'sites', :action=> :intouch
   map.bookmarklet 'bookmarklet', :controller => 'sites', :action => :bookmarklet
-
-  map.toolbar 'toolbar', :controller => 'toolbar'
-
-  map.resources :legislators, :only => [:index], :collection => { :search => :get }
-  map.search 'search', :controller => 'legislators', :action => :search
-
-  map.root :controller => 'sites', :action => :home
+  # Sort of a catch all
+  map.connect ':action', :controller => 'sites'
 
 end
