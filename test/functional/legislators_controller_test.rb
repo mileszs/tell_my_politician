a83af23c93b0d3d88aac31752b5daa0fa5dbfe5d
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-03-14 13:28:27 -0400

    Stub out the dependency on Sunlight API

--- test/functional/legislators_controller_test.rb
+++ test/functional/legislators_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
+require 'factory_girl'
 
 class LegislatorsControllerTest < ActionController::TestCase
-
 
   def setup
     @controller = LegislatorsController.new
@@ -19,12 +19,38 @@ class LegislatorsControllerTest < ActionController::TestCase
   end
 
   context "on GET to :index" do
-    #TODO: Should mock the call to the Sunlight API
-    setup { get :index, :address => '1980 Picadilly Pl. Apt#A Indianapolis, IN 46260' }
+    setup do
+      legislators = {
+        :senior_senator => new_legislator,
+        :junior_senator => new_legislator,
+        :representative => new_legislator
+      }
+      Legislator.stubs(:all_for).returns(legislators)
+      get :index, :address => '1980 Picadilly Pl. Apt#A Indianapolis, IN 46260'
+    end
     should_assign_to :results
     should_render_template :index
     should_not_set_the_flash
     should_respond_with :success
+  end
+
+  private
+
+  def new_legislator(attrs = {})
+    Legislator.new({
+    'title' =>        'Sen',
+    'firstname' =>    'Miles',
+    'middlename' =>   'Zane',
+    'lastname' =>     'Sterrett',
+    'party' =>        'I',
+    'state' =>        'IN',
+    'district' =>     'Junior Seat',
+    'in_office' =>    '1',
+    'gender' =>       'M',
+    'website' =>      'http://mileszs.com',
+    'webform' =>      'http://mileszs.com',
+    'email' =>        'miles.sterrett@gmail.com'
+    }.merge(attrs))
   end
 
 end
