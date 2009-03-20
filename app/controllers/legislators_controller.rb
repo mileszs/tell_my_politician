ee33775664075d0a34a021d91a7f136f5e874a94
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-03-20 15:45:29 -0400

    [425168] Cookies w/ user's ZIP
    Still having issues detecting and redirecting based on the cookies.
    It's frustrating as hell.

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -3,13 +3,27 @@ class LegislatorsController < ApplicationController
   def search
     @link = Link.find_or_create_by_url(params[:link])
     @user = UserLink.create({ :ip => request.remote_ip, :link_id => @link.id })
+    respond_to do |format|
+      if cookies["zip"].to_s.blank?
+        format.html # search.html.erb
+      else
+        format.html { redirect_to legislators_url }
+      end
+    end
   end
 
   def index
-    @results = Legislator.all_for(:address => params[:address])
+    if cookies["zip"].to_s.blank?
+      location = Geocoding::get(params[:address]).first
+      cookies["zip"] = location.postal_code
+      cookies["lat"] = location.latitude.to_s
+      cookies["long"] = location.longitude.to_s
+      @results = Legislator.all_for(:latitude => location.latitude, :longitude => location.longitude)
+    else
+      @results = Legislator.all_for(:latitude => cookies[:lat], :longitude => cookies[:long])
+    end
     respond_to do |format|
       format.html
-      #format.js { }
     end
   end
 end
--- test/functional/legislators_controller_test.rb
+++ test/functional/legislators_controller_test.rb
@@ -10,15 +10,32 @@ class LegislatorsControllerTest < ActionController::TestCase
   end
 
   context "on GET to :search" do
-    setup { get :search, :link => { :url => 'http://google.com' } }
-    should_assign_to :link
-    should_assign_to :user
-    should_render_template :search
-    should_not_set_the_flash
-    should_respond_with :success
+
+    context "without a cookie for ZIP code" do
+      setup do
+        get :search, :link => { :url => 'http://google.com' }
+      end
+      should_assign_to :link
+      should_assign_to :user
+      should_render_template :search
+      should_not_set_the_flash
+      should_respond_with :success
+    end
+
+    context "with a cookie for ZIP code" do
+      setup do
+        @request.cookies['zip'] = CGI::Cookie.new('name' => 'zip', 'value' => '46260')
+        get :search, :link => { :url => 'http://google.com' }
+      end
+      should_assign_to :link
+      should_assign_to :user
+      should_not_set_the_flash
+      should_redirect_to "legislators_url"
+    end
   end
 
   context "on GET to :index" do
+
     setup do
       legislators = {
         :senior_senator => new_legislator,
@@ -28,10 +45,45 @@ class LegislatorsControllerTest < ActionController::TestCase
       Legislator.stubs(:all_for).returns(legislators)
       get :index, :address => '1980 Picadilly Pl. Apt#A Indianapolis, IN 46260'
     end
-    should_assign_to :results
-    should_render_template :index
-    should_not_set_the_flash
-    should_respond_with :success
+
+    context "without a cookie for ZIP code, latitude, or longitude set" do
+      should "have a blank 'zip' cookie" do
+        assert @request.cookies['zip'].blank?
+      end
+      should "have a blank 'lat' cookie" do
+        assert @request.cookies['lat'].blank?
+      end
+      should "have a blank 'long' cookie" do
+        assert @request.cookies['long'].blank?
+      end
+      should_assign_to :results
+      should_render_template :index
+      should_not_set_the_flash
+      should_respond_with :success
+    end
+
+    context "with a cookie for ZIP code, latitude, and longitude set" do
+      setup do
+        @request.cookies['zip'] = CGI::Cookie.new('name' => 'zip', 'value' => '46260')
+        @request.cookies['lat'] = CGI::Cookie.new('name' => 'latitude', 'value' => '39.9209164')
+        @request.cookies['long'] = CGI::Cookie.new('name' => 'zip', 'value' => '-86.1945985')
+      end
+
+      should "not have a blank 'zip' cookie" do
+        assert !@request.cookies['zip'].blank?
+      end
+      should "not have a blank 'lat' cookie" do
+        assert !@request.cookies['lat'].blank?
+      end
+      should "not have a blank 'long' cookie" do
+        assert !@request.cookies['long'].blank?
+      end
+      should_assign_to :results
+      should_render_template :index
+      should_not_set_the_flash
+      should_respond_with :success
+
+    end
   end
 
   private
