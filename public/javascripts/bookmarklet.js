63c6536ba9be2fde6c75a2f0708b7aab9ba0f05f
Donald Perry <dperry@office.(none)>
2009-03-28 19:19:20 -0400

    changes to the make bookmarklet work better

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -10,51 +10,52 @@ var bookmarkletHeight = '257px';
 //we need a place on this page to put everything.
 var ELcontainer = document.createElement('div');
 ELcontainer.id = 'tmp_holder';
-ELcontainer.style.position='absolute';
-ELcontainer.style.top='5px';
-ELcontainer.style.right='0';
-ELcontainer.style.zIndex = 100000;
-ELcontainer.style.padding = '5px';
-ELcontainer.style.backgroundColor = '#000000';
 ELcontainer.style.width = bookmarkletWidth;
 ELcontainer.style.height = '257px';
+
 
 //we need a iframe that will load data from tmp.com
 var ELframe = document.createElement('iframe');
 ELframe.id = 'tmp_iframe';
-ELframe.style.backgroundColor='#FFFFFF';
 ELframe.src = 'http://tellmypolitician.com/search?u='+ CurrentUrl + '&title=' + CurrentTitle + '&client=' + CurrentVersion;
 ELframe.width='100%';
 ELframe.height='100%';
+ELframe.setAttribute('scrolling','no');
 ELcontainer.appendChild(ELframe);
 
 //next we need a div that can hold the logo
 var ELlogo = document.createElement('div');
 ELlogo.id = 'tmp_logo';
-ELlogo.style.background ='url(http://tellmypolitician.com/images/designs/TMP_LOGO.png)';
-ELlogo.style.width = bookmarkletWidth;
-ELlogo.style.height = '100px';
 
 //we need a close button as well
-var ELcloseLink = document.createElement('a');
 var ELcloseImage = document.createElement('img');
-
 ELcloseImage.src = 'http://tellmypolitician.com/images/bookmarklet/close.png';
 
-ELcloseLink.appendChild(ELcloseImage);
-ELlogo.appendChild(ELcloseLink);
+if (window.addEventListener) { 	// Mozilla, Netscape, Firefox
+       ELcloseImage.addEventListener('click', closeBookmarklet, false);
+}
+else {	// IE
+	ELcloseImage.attachEvent('onClick', closeBookmarklet);
+}
 
+ELcontainer.appendChild(ELcloseImage);
 ELcontainer.appendChild(ELlogo);
-
-/*
-ELlogo.style.position='absolute';
-ELlogo.style.top='5px';
-ELlogo.style.right='0';
-ELlogo.style.zIndex = 100000;
-ELlogo.style.padding = '5px';
-ELlogo.style.backgroundColor = '#000000';
-ELlogo.style.width = '489px';
-ELlogo.style.height = '257px';
-*/
-
 document.body.appendChild(ELcontainer);
+
+var ELsheet = document.createElement('style')
+var sheet = "#tmp_holder { position:absolute; top:10px; right:10px; z-index:100000; padding:15px; background-color:#001937; border:5px solid #8F0F30; -moz-border-radius:30px;}";
+sheet += "#tmp_logo{ background:url('http://tellmypolitician.com/images/design/TMP_LOGO.png') no-repeat; width:387px; height:100px; margin:0px auto; position:relative; top:-42px; }";
+sheet += "#tmp_holder img { position:absolute; top:-10px; right:-12px; z-index:10000; }";
+sheet += "#tmp_holder iframe { -moz-border-radius:30px; }";
+ELsheet.innerHTML= sheet;
+document.body.appendChild(ELsheet);
+
+function closeBookmarklet()
+{
+	//ok we need to get rid of our stuff from the page.
+	var ELholder = document.getElementById("tmp_holder");
+	//to do this we first hide it, because other things may not work later?
+        ELholder.style.visibility="hidden";
+	//then we remove it from the page.
+	document.body.removeChild(ELholder);
+}
