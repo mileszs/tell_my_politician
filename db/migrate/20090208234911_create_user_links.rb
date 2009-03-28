a0dad8f3769057ffcd0e667d85788c01e56fce15
Donald Perry <dperry@office.(none)>
2009-03-28 12:50:08 -0400

    work on the bookmarklet look and feel

--- db/migrate/20090208234911_create_user_links.rb
+++ db/migrate/20090208234911_create_user_links.rb
@@ -2,7 +2,7 @@ class CreateUserLinks < ActiveRecord::Migration
   def self.up
     create_table :user_links do |t|
       t.integer :id
-      t.string :ip
+      t.string  :ip
       t.integer :link_id
       t.integer :linkdestination_id
       t.integer :destination_status
--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,6 +1,14 @@
-//first things first we need a place on this page to put everything.
-var ELcontainer = document.createElement('div');
+//lets store all the information that we will need to send.
+var CurrentUrl = escape(document.location.href);
+var CurrentTitle = escape(document.title);
+var CurrentVersion = '1';
 
+//set some constants just so I don't have to change them in 5 places
+var bookmarkletWidth = '489px';
+var bookmarkletHeight = '257px';
+
+//we need a place on this page to put everything.
+var ELcontainer = document.createElement('div');
 ELcontainer.id = 'tmp_holder';
 ELcontainer.style.position='absolute';
 ELcontainer.style.top='5px';
@@ -8,13 +16,10 @@ ELcontainer.style.right='0';
 ELcontainer.style.zIndex = 100000;
 ELcontainer.style.padding = '5px';
 ELcontainer.style.backgroundColor = '#000000';
-ELcontainer.style.width = '489px';
+ELcontainer.style.width = bookmarkletWidth;
 ELcontainer.style.height = '257px';
 
-var CurrentUrl = escape(document.location.href);
-var CurrentTitle = escape(document.title);
-var CurrentVersion = '1';
-
+//we need a iframe that will load data from tmp.com
 var ELframe = document.createElement('iframe');
 ELframe.id = 'tmp_iframe';
 ELframe.style.backgroundColor='#FFFFFF';
@@ -22,5 +27,34 @@ ELframe.src = 'http://tellmypolitician.com/search?u='+ CurrentUrl + '&title=' + 
 ELframe.width='100%';
 ELframe.height='100%';
 ELcontainer.appendChild(ELframe);
-document.body.appendChild(ELcontainer);
 
+//next we need a div that can hold the logo
+var ELlogo = document.createElement('div');
+ELlogo.id = 'tmp_logo';
+ELlogo.style.background ='url(http://tellmypolitician.com/images/designs/TMP_LOGO.png)';
+ELlogo.style.width = bookmarkletWidth;
+ELlogo.style.height = '100px';
+
+//we need a close button as well
+var ELcloseLink = document.createElement('a');
+var ELcloseImage = document.createElement('img');
+
+ELcloseImage.src = 'http://tellmypolitician.com/images/bookmarklet/close.png';
+
+ELcloseLink.appendChild(ELcloseImage);
+ELlogo.appendChild(ELcloseLink);
+
+ELcontainer.appendChild(ELlogo);
+
+/*
+ELlogo.style.position='absolute';
+ELlogo.style.top='5px';
+ELlogo.style.right='0';
+ELlogo.style.zIndex = 100000;
+ELlogo.style.padding = '5px';
+ELlogo.style.backgroundColor = '#000000';
+ELlogo.style.width = '489px';
+ELlogo.style.height = '257px';
+*/
+
+document.body.appendChild(ELcontainer);
