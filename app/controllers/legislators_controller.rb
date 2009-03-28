f96390a3ff68d876822eb987cf0c57fb9beec124
Donald Perry <dperry@office.(none)>
2009-03-28 00:42:21 -0400

    added some comments for clarification in the legislators_controller.rb changed the blog link in application.html.erb changed the bookmarklet.js to work like the wordpress plugin

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,6 +1,15 @@
 class LegislatorsController < ApplicationController
 
   def search
+    #incoming query string looks like
+    #http://tellmypolitician.com/search?u=http%3A//www.w3schools.com/htmldom/prop_doc_title.asp&title=HTML%20DOM%20title%20Property&client=1
+    #we can't rely on the referer to have the page beacuse 
+    #the wordpress plugin displays the link on the 
+    #list page as well as the specific blog post page.
+    # u = The url the user is submitting
+    # title = the title of the page the user is submiting.
+    # client = the id of the client that the user is submitting.
+
     @link = Link.find_or_create_by_url(params[:link])
     @user = UserLink.create({ :ip => request.remote_ip, :link_id => @link.id })
     if !cookies["zip"].to_s.blank?
--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,5 +1,3 @@
-alert('bookmarklet active');
-
 //first things first we need a place on this page to put everything.
 var ELcontainer = document.createElement('div');
 
@@ -13,10 +11,14 @@ ELcontainer.style.backgroundColor = '#000000';
 ELcontainer.style.width = '489px';
 ELcontainer.style.height = '257px';
 
+var CurrentUrl = escape(document.location.href);
+var CurrentTitle = escape(document.title);
+var CurrentVersion = '1';
+
 var ELframe = document.createElement('iframe');
 ELframe.id = 'tmp_iframe';
 ELframe.style.backgroundColor='#FFFFFF';
-ELframe.src = 'http://tellmypolitician.com/';
+ELframe.src = 'http://tellmypolitician.com/search?u='+ CurrentUrl + '&title=' + CurrentTitle + '&client=' + CurrentVersion;
 ELframe.width='100%';
 ELframe.height='100%';
 ELcontainer.appendChild(ELframe);
