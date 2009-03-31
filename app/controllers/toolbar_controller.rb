0723ccd1d95dcc2c29d03e635209bfc1b6175149
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-03-30 22:17:21 -0400

    Passing 'article' url around

--- app/controllers/toolbar_controller.rb
+++ app/controllers/toolbar_controller.rb
@@ -1,18 +1,17 @@
 class ToolbarController < ApplicationController
   def index
-        #the original_url was the page that they were on that they are passing to the legislator
-	@original_url="http://www.google.com" #link.url
+    #the original_url was the page that they were on that they are passing to the legislator
+    @original_url = params[:u]
 
-        #the search_url is our pages that lists their legislators
-        #what happens if cookies aren't enabled? My guess is we don't have to worry about this?
-        @search_url="http://tellmypoliticial.com/" + cookies["zip"]
+    #the search_url is our pages that lists their legislators
+    #what happens if cookies aren't enabled? My guess is we don't have to worry about this?
+    @search_url = "http://tellmypoliticial.com/" + cookies["zip"]
 
-        #the target_url is the legislators contact form.
-	#this will most likely be passed by a query string from the previous page.
-        @target_url= params[:target_url]
+    #the target_url is the legislators contact form.
+    #this will most likely be passed by a query string from the previous page.
+    @target_url= params[:target_url]
 
-        #here we need to go ahead and write out the link_destination.
-
+    #here we need to go ahead and write out the link_destination.
 
   end
 end
