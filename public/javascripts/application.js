4aa953206b81a6ff83d798d3f28be46e68ca617a
Miles Z. Sterrett <miles.sterrett@gmail.com>
2010-05-20 23:58:02 -0400

    Persist the entered address in the search box

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function toggleSearchBoxDefaultValue() {
+  $('#address').focus(function () {
+    if (/Full Address/.test(this.value)) {
+      this.value = '';
+      $(this).addClass('focused');
+    }
+  }).blur(function () {
+    if (this.value == '') {
+      this.value = this.defaultValue;
+      $(this).removeClass('focused');
+    }
+  });
+}
+
+// What Are You Doing? Box.
+jQuery(function() {
+  toggleSearchBoxDefaultValue();
+});
